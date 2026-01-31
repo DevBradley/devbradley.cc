2e5d4ddc77c57a75e0f668874803af60f67539f2
brad <monkeyboihaha@gmail.com>
2026-01-30 21:05:13 -0600

    Fixed subfolder issue and added files

---
+++ devbrad/src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?      family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+  </head>
+
+  <div class="logobig">
+    <img src="/home/deck/Desktop/devbrad1/devbrad/src/assets/logo-big.png">
+
+    <h1> Welcome to my site!</h1>
+
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <h2>projects:</h2>
+    <a href="https://devbradley.github.io/particlebox3beta/">
+      <img style="height: 300px; width: 250px;" src="/home/deck/Desktop/devbrad1/devbrad/src/assets/card1.png">
+    </a>
+
+    <a href="https://devbradley.cc">
+      <img style="height: 300px; width: 250px;" src="/home/deck/Desktop/devbrad1/devbrad/src/assets/card2.png">
+    </a>
+
+
+  </div>
+
+</template>
+
+
+<style>
+  body {
+    position: absolute;
+    font-family: "Source Code Pro", monospace;
+    background-color: #262626;
+  }
+
+
+
+  .logobig {
+
+  }
+
+  .logobig img {
+    height: 50px;
+    width: 200px;
+  }
+
+
+</style>
